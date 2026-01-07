ceaa8702118b425487b2f8744bac323f99b99e3b
Chengran Sun <ryan.c.sun.ca@gmail.com>
2026-01-07 16:06:26 -0500

    customized some CSS style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">Hello!</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">press me</Button>
+        </div>
+        <div>
+          <Input placeholder="input here please"></Input>
+        </div>
+        <div>
+          <Progress value={30}></Progress>
+        </div>
+        <div>
+          <Textarea placeholder="text here"></Textarea>
+        </div>
+        <div>
+          <Checkbox></Checkbox>
+        </div>
+      </div>
     </div>
   );
 }
